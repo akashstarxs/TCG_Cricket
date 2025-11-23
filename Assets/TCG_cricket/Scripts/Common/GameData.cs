30bf311e9291c600402bb8f875a6bc5b5dc9c0c9
Akash SK <akashoffical23@gmail.com>
2025-11-23 14:02:10 +0530

    Defined Gameplay states

--- Assets/TCG_cricket/Scripts/Common/GameData.cs
+++ Assets/TCG_cricket/Scripts/Common/GameData.cs
@@ -4,6 +4,71 @@ namespace Tcg_cricket
 {
     public class GameData : MonoBehaviour
     {
+        #region Game States
+
+        public enum GameState
+        {
+            None,
+            Lobby,              // Waiting for 2 players to join
+            MatchStart,         // Decks created, initial hand drawn
+
+            TurnStart,          // Start of each turn: +1 draw, cost update
+            PlayerAction,       // Players selecting cards (30s timer)
+            WaitingForEnd,      // One player ended, waiting for the other
+
+            Reveal,             // Both players' cards revealed
+            Resolve,            // Base power + abilities processed
+
+            TurnEnd,            // Cleanup, increment turn
+            MatchEnd            // Winner decided after 6 turns
+        }
+        public enum PlayerState
+        {
+            None,
+            Drawing,            // Start turn: drawing + setting cost
+            Selecting,          // Player choosing cards
+            EndedTurn,          // Player pressed End Turn
+            Locked              // During reveal/resolve
+        }
         
+        public enum TurnPhase
+        {
+            None,
+            Select,             // PlayerAction
+            Reveal,             // Reveal phase
+            Resolve,            // Server applies abilities
+            Cleanup             // Prepare next turn
+        }
+        
+        public enum NetAction
+        {
+            GameStart,
+            TurnStart,
+            PlayCards,
+            EndTurn,
+            RevealCards,
+            GameEnd,
+            FullState
+        }
+        public enum AbilityType
+        {
+            None,
+            GainPoints,                 // +value to score
+            StealPoints,                // steal value points
+            DoublePower,                // multiply power * value
+            DrawExtraCard,              // draw N extra cards
+            DiscardOpponentRandomCard,  // remove N from opponent hand
+            DestroyOpponentCardInPlay   // remove N from opponent reveal zone
+        }
+        
+        public enum UIState
+        {
+            None,
+            Waiting,
+            SelectingCards,
+            ShowingReveal,
+            ShowingResult
+        }
+        #endregion
     }
 }
